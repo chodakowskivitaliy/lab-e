65a85b074b7779dee88c1996e8c923db78dc3b5c
Vitaliy Chodakowski <cv49224@zut.edu.pl>
2023-12-04 15:06:36 +0100

    Update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {RouterOutlet} from "@angular/router";
+import { TasksComponent } from './tasks/tasks.component';
+import {HttpClientModule} from "@angular/common/http";
+import {AppRoutingModule} from "./app-routing.module";
+import {FormsModule} from "@angular/forms";
+import { ArchiveComponent } from './archive/archive.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TasksComponent,
+    ArchiveComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+    imports: [
+        BrowserModule,
+        RouterOutlet,
+        AppRoutingModule,
+        HttpClientModule,
+        FormsModule,
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
